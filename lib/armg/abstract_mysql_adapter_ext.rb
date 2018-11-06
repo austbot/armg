297d24c42cea456270a000ce93c676f40a314b1e
Austin Adams <austbot@users.noreply.github.com>
2018-11-05 16:35:07 -0800

    Update abstract_mysql_adapter_ext.rb

--- lib/armg/abstract_mysql_adapter_ext.rb
+++ lib/armg/abstract_mysql_adapter_ext.rb
@@ -1,18 +1,6 @@
 module Armg::AbstractMysqlAdapterExt
-  def initialize_type_map(m)
+  def initialize_type_map(m = type_map)
     super
     m.register_type %r(^geometry)i, Armg::MysqlGeometry.new
-  end
-
-  def indexes(*args, &block)
-    is = super
-
-    is.each do |i|
-      if i.type == :spatial
-        
-      end
-    end
-
-    is
   end
 end
